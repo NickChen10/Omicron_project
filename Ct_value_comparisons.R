468193816e969e6514895ab676661e0a9d52e353
NickChen10 <nicholas.chen@yale.edu>
2023-07-11 11:09:29 -0400

    Added ct overview figure

--- Ct_value_comparisons.R
+++ Ct_value_comparisons.R
@@ -9,6 +9,8 @@ library(snakecase)
 library(plotly)
 library(ggsignif)
 library(cowplot)
+library(ggview)
+library(patchwork)
 
 
 ##### DATA FORMATTING #####
@@ -26,7 +28,7 @@ data[,c("extraction_pcr_date","collection_date")]<-lapply(data[,c("extraction_pc
 data <- data %>% filter(filter=="connecticut") %>% select(sample_id,collection_date,yale_n_1_fam,lineage, n1_ge_ml) %>% rename(n1 = yale_n_1_fam) 
 
 
-#Recode lineages 
+#Recode lineages with aggregated lineages
 data <- data %>%
   mutate(ShortLin = case_when(
     endsWith(lineage, "2.12.1") ~ "BA.2",
@@ -155,11 +157,11 @@ bounds<- as.Date(c((cross_overs[1]-14),(cross_overs[1]+14),(cross_overs[2]-14),(
 
 
 ###### CT VALUE STATISTICAL TESTS ######
-data <- data %>% filter(ShortLin != "Other")
+data_stats <- data %>% filter(ShortLin != "Other")
 
 # Define intervals to test
 for(i in c(1,3,5)){
-  interval <- data %>% filter(collection_date >= bounds[i] & collection_date <= bounds[i+1])
+  interval <- data_stats %>% filter(collection_date >= bounds[i] & collection_date <= bounds[i+1])
   assign(paste0("interval_",c(1,1,2,2,3)[i]),interval)
 }
 
@@ -282,3 +284,210 @@ p3 <- ggplot(interval_3,aes(x=ShortLin,y=n1, color=ShortLin))+
             check_overlap = TRUE, color="black")
 
 plot_grid(p1,p2,p3,nrow=1)
+
+
+
+###### CT VALUE LINEAR REGRESSIONS ######
+
+
+#Recode lineages including BA.1.1 and BA.2.12.1
+data$ShortLin <- ""
+lineages <- data %>% 
+  mutate(ShortLin = case_when(
+    endsWith(lineage, "2.12.1") ~ "BA.2.12.1",
+    startsWith(lineage, "Q") ~ "Alpha",
+    startsWith(lineage, "B.1.1.7") ~ "Alpha",
+    startsWith(lineage, "AY") ~ "Delta",
+    startsWith(lineage, "B.1.617") ~ "Delta",
+    startsWith(lineage, "BG") ~ "BA.2",
+    startsWith(lineage, "BA.2.75") ~ "BA.2",
+    startsWith(lineage, "BA.1.1") ~ "BA.1.1",
+    startsWith(lineage, "BA.1") ~ "BA.1",
+    startsWith(lineage, "BA.2") ~ "BA.2",
+    startsWith(lineage, "BK") ~ "BA.2",
+    startsWith(lineage, "BP") ~ "BA.2",
+    startsWith(lineage, "BL") ~ "BA.2",
+    startsWith(lineage, "BH") ~ "BA.2",
+    startsWith(lineage, "BM") ~ "BA.2",
+    startsWith(lineage, "CH") ~ "BA.2",
+    startsWith(lineage, "BJ") ~ "BA.2",
+    startsWith(lineage, "BR") ~ "BA.2",
+    startsWith(lineage, "BA.4.6") ~ "BA.4.6",
+    startsWith(lineage, "BA.4") ~ "BA.4",
+    startsWith(lineage, "BA.5") ~ "BA.5",
+    startsWith(lineage, "BU") ~ "BA.5",
+    startsWith(lineage, "BV") ~ "BA.5",
+    startsWith(lineage, "BE") ~ "BA.5",
+    startsWith(lineage, "BT") ~ "BA.5",
+    startsWith(lineage, "BF.7") ~ "BA.5",
+    startsWith(lineage, "BF") ~ "BA.5", #BF.7 as BA.5
+    startsWith(lineage, "BQ.1.1") ~ "BQ.1.1",
+    startsWith(lineage, "BQ.2") ~ "BA.5",
+    startsWith(lineage, "BQ.3") ~ "BA.5",
+    startsWith(lineage, "CN") ~ "BA.5",
+    startsWith(lineage, "CD") ~ "BA.5",
+    startsWith(lineage, "CE") ~ "BA.5",
+    startsWith(lineage, "CF") ~ "BA.5",
+    startsWith(lineage, "CL") ~ "BA.5",
+    startsWith(lineage, "CG") ~ "BA.5",
+    startsWith(lineage, "CK") ~ "BA.5",
+    startsWith(lineage, "BZ") ~ "BA.5",
+    startsWith(lineage, "XBB.1.5") ~ "XBB.1.5",
+    startsWith(lineage, "XBB.1") ~ "XBB.1.5", 
+    TRUE ~ "Other"
+  ))
+
+lineages$ShortLin <- factor(lineages$ShortLin, levels=c("Other","Alpha","Delta","BA.1","BA.1.1","BA.2","BA.2.12.1","BA.4.6","BA.5","BQ.1.1","XBB.1.5"))
+
+
+
+
+# Group data by week
+lineages <- lineages %>% mutate(CopyDate = collection_date)%>% group_by(`week` = cut(`CopyDate`, "week"))
+lineages$week <- as.Date(lineages$week)
+lineages <- lineages[!is.na(lineages$`week`),] #delete data without week assigned
+
+
+# Variant counts by week for emergence periods (CLEAN THIS USING MY DATA_FREQ CODE)
+lineages_sum <- lineages %>% group_by(week) %>% count(ShortLin)
+lineages_count  <- aggregate(lineages_sum$n, by=list(lineages_sum$week), FUN = sum)
+colnames(lineages_count) <- c("week","Total")
+lineages_sum  <- right_join(lineages_sum, lineages_count, by = "week")
+lineages_sum <- lineages_sum %>% mutate(`freq` = ((`n`/`Total`)*100))
+
+
+
+# First half of variant scatterplots
+variants_1sthalf <- c("BA.1","BA.1.1","BA.2","BA.2.12.1","BA.5")
+range <- list()
+variant_data <- list()
+
+# Define range of time when the variant is above 10% in the population 
+for (i in variants_1sthalf) {
+  range[[i]] <- range(lineages_sum$week[which(lineages_sum$ShortLin == i & lineages_sum$freq >= 10)])
+  variant_data[[i]] <- lineages %>% filter(ShortLin == i) %>%
+    filter(collection_date  >= min(range[[i]]) & 
+             collection_date <= max(range[[i]]))
+}
+
+
+variants_graphs_1st_half <- list()
+
+
+# Plot the first half of the figure 
+OmiCols <- c("BA.1" = "#74875B", 
+             "BA.1.1"= "#184E27",
+             "BA.2" = "#FB7069",
+             "BA.2.12.1" = "#CE4E50",
+             "BA.4" = "#CAC6EF",
+             "BA.4.6" = "#CAC6EF",
+             "BA.5" = "#7294D4",
+             "BQ.1.1" = "#184294",
+             "XBB" = "#892F2E",
+             "XBB.1" = "#892F2E",
+             "XBB.1.5" = "#892F2E",
+             "Others" = "grey"
+)
+
+for (i in variants_1sthalf) {
+  variants_graphs_1st_half[[i]] <- ggplot(variant_data[[i]], 
+                                          aes(x=collection_date, y=n1, 
+                                              color=ShortLin, group=ShortLin)) +
+    geom_point(size=0.8,alpha=0.5)+
+    geom_smooth(method = "lm", se=T, aes(group=ShortLin, fill=ShortLin),size=1, alpha=0.1)+
+    scale_y_reverse(breaks=c(15,25,35))+  
+    ylab("Ct N")+
+    theme_classic()+
+    theme(
+      axis.text.x=element_text(face = "bold"),
+      axis.title.x=element_blank(),
+      legend.box.background = element_rect(colour = "black"),
+      legend.position = "none")+
+    scale_fill_manual(values = c(OmiCols))+
+    scale_color_manual(values = c(OmiCols))+
+    coord_fixed(xlim= c(as.Date("2022-01-10"),as.Date("2022-07-20")), ylim = c(35,15))
+}
+
+# Repeat process for the second half of the figure
+variants_2ndhalf <- c("BA.5","BA.4.6","BQ.1.1","XBB.1.5")
+
+range_2 <- list()
+variant_data_2 <- list()
+
+for (i in variants_2ndhalf) {
+  range_2[[i]] <- range(lineages_sum$week[which(lineages_sum$ShortLin == i & lineages_sum$freq >= 10)])
+  variant_data_2[[i]] <- lineages %>% filter(ShortLin == i) %>%
+    filter(collection_date  >= min(range_2[[i]]) & 
+             collection_date <= max(range_2[[i]]))
+}
+
+variants_graphs_2nd_half <- list()
+
+for (i in variants_2ndhalf) {
+  
+  variants_graphs_2nd_half[[i]] <- ggplot(variant_data_2[[i]] %>% filter(`ShortLin` == i)
+                                          , aes(x=collection_date, y=n1, 
+                                                color=ShortLin, group=ShortLin)) +
+    geom_point(size=0.8,alpha=0.5)+
+    geom_smooth(method = "lm", se=T, aes(group=ShortLin, fill=ShortLin),size=1, alpha=0.1)+
+    #scale_x_date(breaks="1 months")+
+    scale_y_reverse(breaks=c(15,25,35))+    
+    #ylab("Ct N")+
+    theme_classic()+
+    theme(
+      axis.text.x=element_text(face = "bold"),
+      axis.title.x=element_blank(),
+      axis.title.y=element_blank(),
+      legend.box.background = element_rect(colour = "black"),
+      legend.position = "none")+
+    scale_fill_manual(values = c(OmiCols))+
+    scale_color_manual(values = c(OmiCols))+
+    coord_fixed(xlim= c(as.Date("2022-07-20"),as.Date("2023-01-20")), ylim = c(35,15))
+}
+
+
+# Combine data into one plot
+variants_combined <- rbind(variant_data[["BA.1"]],
+                           variant_data[["BA.1.1"]],
+                           variant_data[["BA.2"]],
+                           variant_data[["BA.2.12.1"]],
+                           variant_data[["BA.5"]],
+                           variant_data_2[["BA.5"]],
+                           variant_data_2[["BA.4.6"]],
+                           variant_data_2[["BQ.1.1"]],
+                           variant_data_2[["XBB.1.5"]]
+)
+
+# Combine linear regressions
+overview <- ggplot(variants_combined, aes(x=collection_date, y=n1, color=ShortLin, group=ShortLin)) +
+  geom_hline(yintercept = 25,colour = "lightgrey")+
+  geom_point(size=0.2,alpha=0.7, color="white",show.legend=FALSE)+
+  geom_smooth(method = "lm", se=T, aes(group=ShortLin, fill=ShortLin),size=1.5, alpha=0.1,show.legend=FALSE)+
+  scale_y_reverse()+
+  coord_cartesian(ylim = c(29, 22))+
+  xlab("Collection Date")+
+  ylab("Ct N")+
+  labs(
+    title = "Viral load measured via qPCR Ct nucleocapsid (N) target")+
+  theme_classic()+
+  theme(
+    axis.text.x=element_text(size = 12, face ="bold"),
+    axis.text.y=element_text(size = 12, face ="bold"),
+    axis.title = element_text(size = 12, face ="bold"),
+    plot.title = element_text(size = 15, face ="bold")
+  )+
+  scale_fill_manual(values = c(OmiCols))+
+  scale_color_manual(values = c(OmiCols))
+overview
+ggsave(filename="CToverview.png", units = "cm", width = 30, height = 10, scale =0.7)
+
+
+# Plot scatterplots together
+(variants_graphs_1st_half[[1]] + plot_spacer() +
+   variants_graphs_1st_half[[2]] + variants_graphs_2nd_half[[2]] + 
+   variants_graphs_1st_half[[3]]+ variants_graphs_2nd_half[[3]] +
+   variants_graphs_1st_half[[4]]+ variants_graphs_2nd_half[[4]]+ 
+   variants_graphs_1st_half[[5]]+  variants_graphs_2nd_half[[1]]  +
+   plot_layout(ncol = 2))
+ggsave(filename="variantCT.png", units = "cm", scale = 1.4)
+
